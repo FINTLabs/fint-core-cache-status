f45b97aa4f957283551e463e3ab4636b3e8f5eb8
sindre <sindre.giske@novari.no>
2025-07-08 14:21:00 +0200

    CacheService refactor

--- src/main/kotlin/no/fintlabs/CacheService.kt
+++ src/main/kotlin/no/fintlabs/CacheService.kt
@@ -1,9 +1,6 @@
 package no.fintlabs
 
 import jakarta.annotation.PostConstruct
-import no.fintlabs.model.CacheStatus
-import no.fintlabs.model.CacheResponse
-import no.fintlabs.model.StatusInfo
 import org.springframework.stereotype.Service
 import java.time.Duration
 import java.time.Instant
@@ -12,40 +9,32 @@ import java.time.Instant
 class CacheService(
     private val cacheClient: CacheClient
 ) {
-    private var cacheResponse: CacheResponse? = null
+    private fun Instant.getStatusFromTime(): Int =
+        Duration.between(this, Instant.now()).toStatusValue()
 
-    fun initCacheStatus() {
-        val response = cacheClient.getCache("https://api.felleskomponent.no/utdanning/vurdering")
-        cacheResponse = response
-    }
+    private fun Duration.toStatusValue(): Int =
+        when {
+            this > Duration.ofHours(1) -> -1
+            this > Duration.ofMinutes(30) -> 0
+            else -> 1
+        }
 
     @PostConstruct
-    fun getStatus(): CacheStatus {
-        initCacheStatus()
-
-        val orgs: MutableMap<String, MutableMap<String, StatusInfo>> = mutableMapOf()
-
-        for ((orgName, resources) in cacheResponse!!.orgs) {
-            val resourceMap: MutableMap<String, StatusInfo> = mutableMapOf()
-
-            for ((resourceName, resourceInfo) in resources) {
-                if (resourceInfo.size != 0) {
-                    val size: Int = resourceInfo.size
-                    val sinceLastUpdated: Duration = Duration.between(resourceInfo.lastUpdated, Instant.now())
-                    val status: Int = when {
-                        sinceLastUpdated > Duration.ofMinutes(30) -> 1
-                        sinceLastUpdated > Duration.ofHours(1) -> 0
-                        else -> -1
+    fun getStatus() =
+        listOf("utdanning/vurdering").forEach {
+            cacheClient.getCache("https://api.felleskomponent.no/${it.replace("-", "/")}")
+                ?.let {
+                    it.orgs.forEach { org, resourceMap ->
+                        resourceMap.forEach { resource, resourceInfo ->
+                            if (resourceInfo.size > 0) {
+                                println(
+                                    "$org - $resource : ${
+                                        resourceInfo.lastUpdated.getStatusFromTime()
+                                    }"
+                                )
+                            }
+                        }
                     }
-                    resourceMap[resourceName] = StatusInfo(status = status, size = size)
                 }
-            }
-            if (resourceMap.isNotEmpty()) {
-                orgs[orgName] = resourceMap
-                println(resourceMap)
-            }
         }
-        println(CacheStatus(orgs = orgs))
-        return CacheStatus(orgs = orgs)
-    }
 }
