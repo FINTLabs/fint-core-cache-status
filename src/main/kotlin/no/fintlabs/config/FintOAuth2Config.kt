33f99a2674025e1774d8fbb3c34d00eca00415b6
sindre <sindre.giske@novari.no>
2025-07-11 14:59:54 +0200

    [FEAT] DONE

--- src/main/kotlin/no/fintlabs/config/FintOAuth2Config.kt
+++ src/main/kotlin/no/fintlabs/config/FintOAuth2Config.kt
@@ -3,7 +3,7 @@ package no.fintlabs.config
 import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties(prefix = "fint.security.oauth2.client")
-data class FintOAuth2Config (
-     val username: String,
-     val password: String,
+data class FintOAuth2Config(
+    val username: String,
+    val password: String,
 )
